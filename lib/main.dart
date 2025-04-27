b2296150dccc908e615c8d18a55ddb68559069f3
Mahmoud Assasa <54775606+mahmoudassasa@users.noreply.github.com>
2025-04-28 00:32:44 +0300

    - just a fake push to origin development

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:book_a_doctor/core/routing/app_router.dart';
 import 'package:book_a_doctor/doc_app.dart';
 import 'package:flutter/material.dart';
 
-void main() { 
+void main() {
+   
   runApp( DocApp(appRouter: AppRouter(),));
 }
